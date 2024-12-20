78efc37516f924c1ec7bbccbbad6d7134e030d48
keonly <70371393+keonly@users.noreply.github.com>
2024-12-20 18:58:03 +0900

    chore: remove irrelevant comment

--- home/keon/zaurak.nix
+++ home/keon/zaurak.nix
@@ -10,7 +10,5 @@
     ./darwin
   ];
 
-  # For some bizarre reasons, $XDG_ environment variable is not set in zsh but is properly set in fish.
-  # While it is possible to set those in zsh also, I was too lazy and decided to stick with this stopgap.
   programs.kitty.settings.shell = "zsh --login -c 'nu --login --interactive'";
 }
