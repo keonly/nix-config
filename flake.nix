7b8f0f8878d271bf91c964c4f616bdce2e92e40d
keonly <70371393+keonly@users.noreply.github.com>
2024-12-23 17:35:36 +0900

    fix: use unstable packages

--- flake.nix
+++ flake.nix
@@ -4,14 +4,8 @@
   inputs = {
     # Nixpkgs
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-24.11";
-    };
-    # You can access packages and modules from different nixpkgs revs
-    # at the same time. Here's an working example:
-    nixpkgs-unstable = {
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
-    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Nix-Darwin
     nix-darwin = {
@@ -21,7 +15,10 @@
 
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -38,33 +35,34 @@
         inherit self inputs;
       };
     });
-    # Supported systems for your flake packages, shell, etc.
     systems = [
       "x86_64-linux"
       "aarch64-darwin"
     ];
-    # This is a function that generates an attribute by calling a function you
-    # pass to it, with each system as an argument
+
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
     # Your custom packages
     # Accessible through 'nix build', 'nix shell', etc
     packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+
     # Formatter for your nix files, available through 'nix fmt'
     # Other options beside 'alejandra' include 'nixpkgs-fmt'
     formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
     # Your custom packages and modifications, exported as overlays
     overlays = import ./overlays {inherit inputs;};
+
     # Reusable nixos modules you might want to export
     # These are usually stuff you would upstream into nixpkgs
     nixosModules = import ./modules/nixos;
+
     # Reusable home-manager modules you might want to export
     # These are usually stuff you would upstream into home-manager
     homeManagerModules = import ./modules/home-manager;
 
     # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
+    # Available through 'nixos-rebuild --flake .#hostname'
     nixosConfigurations = {
       alioth = lib.custom.mkNixosConfig {
         hostname = "alioth";
@@ -72,7 +70,7 @@
     };
 
     # nix-darwin configuration entrypoing
-    # Available through 'darwin-rebuild switch --flake .#$your_hostname'
+    # Available through 'darwin-rebuild switch --flake .#hostname'
     darwinConfigurations = {
       "zaurak" = lib.custom.mkDarwinConfig {
         username = "keon";
@@ -82,7 +80,7 @@
     };
 
     # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
+    # Available through 'home-manager switch --flake .#username@hostname'
     homeConfigurations = {
       "keon@alioth" = lib.custom.mkHomeConfig {
         username = "keon";
