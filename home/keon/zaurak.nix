16e9c0e38a0031b17ba542300a43e9f5fa0bd9d5
keonly <70371393+keonly@users.noreply.github.com>
2024-12-23 12:04:14 +0900

    fix: set tmux default shell to fish

--- home/keon/zaurak.nix
+++ home/keon/zaurak.nix
@@ -11,4 +11,8 @@
   ];
 
   programs.kitty.settings.shell = "zsh --login -c 'fish --login --interactive'";
+  programs.tmux.extraConfig = lib.mkAfter ''
+    set -gu default-command
+    set -g default-shell /run/current-system/sw/bin/fish
+  '';
 }
