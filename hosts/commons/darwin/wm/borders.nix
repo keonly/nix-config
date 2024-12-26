edbf69790e2cd192a3ea77ed3383169f766f0505
keonly <70371393+keonly@users.noreply.github.com>
2024-12-26 14:32:27 +0900

    feat(darwin): add jankyborders

---
+++ hosts/commons/darwin/wm/borders.nix
@@ -0,0 +1,11 @@
+{...}: {
+  services.jankyborders = {
+    enable = true;
+
+    style = "round";
+    hidpi = true;
+    width = 6.0;
+    active_color = "0xf4f4f4ff";
+    inactive_color = "0x00000000";
+  };
+}
