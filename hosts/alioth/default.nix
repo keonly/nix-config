fdaf49b0364961f9bfe7e6e2ba27bc80df880580
keonly <70371393+keonly@users.noreply.github.com>
2025-02-02 00:14:20 +0900

    refactor: move cli to util

--- hosts/alioth/default.nix
+++ hosts/alioth/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ../commons/global
+    ../commons/optional/util/nh.nix
     ./configuration.nix
   ];
   nix.settings.experimental-features = ["nix-command" "flakes"];
--- hosts/commons/global/default.nix
+++ hosts/commons/global/default.nix
@@ -1,6 +1,5 @@
 {...}: {
   imports = [
     ./font
-    ./nix
   ];
 }
