ab4b3710d79a9c24aa969b4fd307e18503c566e5
keonly <70371393+keonly@users.noreply.github.com>
2025-05-13 14:31:11 +0900

    chore(home): move ghostty

---
+++ home/common/programs/ghostty.nix
@@ -0,0 +1,64 @@
+{
+  pkgs ? import <nixpkgs> {},
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    ghostty.enable = lib.mkEnableOption "Whether to enable ghostty";
+  };
+
+  config = lib.mkIf config.ghostty.enable {
+    programs.ghostty = {
+      enable = true;
+      package =
+        if pkgs.stdenv.isDarwin
+        then null # pkgs.ghostty is broken on darwin
+        else pkgs.ghostty;
+
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+
+      settings = {
+        font-family = "Iosevka Nerd Font Mono";
+        font-size = 16;
+        theme = "carppuccin-mocha";
+
+        # https://ghostty.org/docs/config/reference#command
+        #  To resolve issues:
+        #    1. https://github.com/ryan4yin/nix-config/issues/26
+        #    2. https://github.com/ryan4yin/nix-config/issues/8
+        #  Spawn a nushell in login mode via `bash`
+        # command = "${pkgs.bash}/bin/bash --login -c 'nu --login --interactive'";
+      };
+
+      themes = {
+        carppuccin-mocha = {
+          background = "161616";
+          cursor-color = "f5e0dc";
+          foreground = "f4f4f4";
+          palette = [
+            "0=#393939"
+            "1=#f38ba8"
+            "2=#a6e3a1"
+            "3=#f9e2af"
+            "4=#89b4fa"
+            "5=#f5c2e7"
+            "6=#94e2d5"
+            "7=#e0e0e0"
+            "8=#525252"
+            "9=#f38ba8"
+            "10=#a6e3a1"
+            "11=#f9e2af"
+            "12=#89b4fa"
+            "13=#f5c2e7"
+            "14=#94e2d5"
+            "15=#c6c6c6"
+          ];
+          selection-background = "353749";
+          selection-foreground = "f4f4f4";
+        };
+      };
+    };
+  };
+}
