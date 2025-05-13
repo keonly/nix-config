cc351f55217d412155e8ff7f0471d74390fe671e
keonly <70371393+keonly@users.noreply.github.com>
2025-05-13 14:33:46 +0900

    feat(home): add jankyborders

---
+++ home/darwin/services/jankyborders.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    jankyborders.enable = lib.mkEnableOption "Whether to enable jankyborders";
+  };
+
+  config = lib.mkIf config.jankyborders.enable {
+    services.jankyborders = {
+      enable = true;
+
+      settings = {
+        style = "round";
+        width = 8.0;
+        active_color = "0xf4f4f4ff";
+        inactive_color = "0x00000000";
+      };
+    };
+  };
+}
