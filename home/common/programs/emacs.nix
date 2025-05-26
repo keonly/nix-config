c5f7a4bfc5a595de3661e7958cb297b776f9b136
keonly <keon.lee@proton.me>
2025-05-26 09:32:17 +0900

    feat: install lsp servers for emacs

--- home/common/programs/emacs.nix
+++ home/common/programs/emacs.nix
@@ -14,8 +14,56 @@
       enable = true;
       emacs = pkgs.emacs-pgtk;
       doomDir = doom-config;
+      experimentalFetchTree = true;
     };
 
-    services.emacs.enable = true;
+    services.emacs = {
+      enable = true;
+      client = {
+        enable = true;
+        arguments = ["-n -c"];
+      };
+      socketActivation.enable = true;
+    };
+
+    # LSP Servers
+    home.packages = with pkgs; [
+      # C / C++
+      clang-tools
+
+      # Go
+      gopls
+
+      # Javascript / Typescript / JSON / CSS / HTML
+      nodePackages.typescript-language-server
+      nodePackages.vscode-langservers-extracted
+
+      # Kotlin
+      kotlin-language-server
+
+      # LaTeX
+      texlab
+
+      # Lua
+      lua-language-server # attribute name on 24.11-unstable; aliases: sumneko-…
+
+      # Nix
+      nil
+
+      # Python
+      pyright
+
+      # Rust
+      rust-analyzer
+
+      # Scala
+      metals
+
+      # Bash
+      nodePackages.bash-language-server
+
+      # YAML
+      yaml-language-server
+    ];
   };
 }
