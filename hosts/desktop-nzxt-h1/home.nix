5b04704c71fd34e09b94665a1ae5d8c9b47556f3
keonly <70371393+keonly@users.noreply.github.com>
2025-06-02 17:05:23 +0900

    feat(nzxt-h1): enable spotify-player

--- hosts/desktop-nzxt-h1/home.nix
+++ hosts/desktop-nzxt-h1/home.nix
@@ -16,6 +16,7 @@
   nh.enable = true;
   nushell.enable = true;
   ripgrep.enable = true;
+  spotify-player.enable = true;
   starship.enable = true;
   thefuck.enable = true;
   vesktop.enable = true;
