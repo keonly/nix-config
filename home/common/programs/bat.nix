948f21cb01e74af9027066eee4a45b16b4c378a7
keonly <70371393+keonly@users.noreply.github.com>
2025-06-09 16:13:51 +0900

    fix(home/bat): proper theming

--- home/common/programs/bat.nix
+++ home/common/programs/bat.nix
@@ -20,7 +20,12 @@
             rev = "007a093995118e1b8aeba2ec9f9db1f785038aab";
             sha256 = "sha256-xwXhEaKfjNuiiWsBwGiShxxzQGDhM677QiIysAY1bKU=";
           };
+          file = "themes/Catppuccin\ Mocha.tmTheme";
         };
+      };
+
+      config = {
+        theme = "carppuccin-mocha";
       };
     };
   };
