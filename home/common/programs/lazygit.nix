6613aeea8f6f869af94111dd6c5ba1ed3193807c
keonly <70371393+keonly@users.noreply.github.com>
2025-06-11 11:38:06 +0900

    feat(home/lazygit): update config
    - use delta as pager
    - disable catppuccin and use custom colourscheme

--- home/common/programs/lazygit.nix
+++ home/common/programs/lazygit.nix
@@ -8,8 +8,36 @@
   };
 
   config = lib.mkIf config.lazygit.enable {
+    catppuccin.lazygit.enable = false;
+
     programs.lazygit = {
       enable = true;
+
+      settings = {
+        gui = {
+          nerdFontsVersion = "3";
+
+          theme = {
+            activeBorderColor = ["#f4f4f4" "bold"];
+            cherryPickedCommitBgColor = ["#f2cdcd"];
+            cherryPickedCommitFgColor = ["#89b4fa"];
+            defaultFgColor = ["#f4f4f4"];
+            inactiveBorderColor = ["#8d8d8d"];
+            optionsTextColor = ["#89b4fa"];
+            searchingActiveBorderColor = ["#393939" "bold"];
+            selectedLineBgColor = ["#393939"];
+            unstagedChangesColor = ["#f38ba8"];
+          };
+        };
+
+        git = {
+          paging = {
+            colorArg = "always";
+            pager = "delta --dark --paging=never";
+            useConfig = false;
+          };
+        };
+      };
     };
   };
 }
